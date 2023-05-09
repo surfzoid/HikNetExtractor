5bac076c74ae56ff6e02783be023eff6703c5785
surfzoid <surfzoid@gmail.com>
2023-05-09 16:03:49 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import os
+
+setup(
+        name = 'HikNetExtractor',
+        version = '1.0.10',
+        packages = ['HikNetExtractor'],
+        scripts = ['HikNetExtractor'],
+        license = 'GNU General Public License v3 (GPLv3)',
+        url = 'https://github.com/surfzoid/HikNetExtractor',
+        description = 'Provides functionality to extract periodically record event from Hikvion camera or NVR.',
+        long_description = 'Provides functionality to extract periodically record event from Hikvion camera or NVR with ISAPI and HTTPDigestAuth enable. Add this script to an schedule task and you will keep records during the number of day you put in the config.',
+        platforms = ['Linux, Windows, Mac'],
+        author = 'Surfzoid',
+        author_email = 'surfzoid@gmail.com',
+        maintainer = 'Surfzoid',
+        maintainer_email = 'surfzoid@gmail.com',
+        )
